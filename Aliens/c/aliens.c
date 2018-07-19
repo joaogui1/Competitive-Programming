bdefe9c190b9e2e6d77b448a5bb7b937cb38cd9a
john <joaoguilhermearujo@gmail.com>
2018-07-19 18:31:34 -0300

    Removi algumas pastas

--- Aliens/c/aliens.c
+++
@@ -1,5 +0,0 @@
-#include "aliens_c.h"
-
-long long take_photos(int n, int m, int k, int* r, int* c) {
-    return 0;
-}
--- Aliens/cpp/aliens.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <vector>
-
-long long take_photos(int n, int m, int k, std::vector<int> r, std::vector<int> c);
-
